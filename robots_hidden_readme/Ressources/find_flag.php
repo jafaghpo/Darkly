5d508be7d5e6acb7ad0cc021a9bdc7e97193e0a6
Guillaume GREGOIRE <ggregoir@student.42.fr>
2020-01-22 14:26:56 +0100

    all flags done

---
+++ robots_hidden_readme/Ressources/find_flag.php
@@ -0,0 +1,34 @@
+<?php
+$array = array();
+$regex = '/<a href="(.+)">/';
+
+function find_flag($base_url)
+{
+  global $array;
+  global $regex;
+
+  $content = file_get_contents($base_url);
+  preg_match_all($regex, $content, $matches);
+  $urls = $matches[1];
+
+  foreach($urls as $url) 
+  {
+    if ($url == "../")
+      continue ;
+    if ($url == "README")
+    {
+      $c = file_get_contents($base_url.$url);
+      if ( !in_array($c, $array) )
+      {
+        print_r($base_url.$url." => ".$c);
+        $array[$c] = $c;
+      }
+    }
+    else
+    {
+      find_flag($base_url.$url);
+    }
+  }
+}
+find_flag("http://10.11.200.28/.hidden/");
+?>
